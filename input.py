14eed91091d6b266c11d1cf5fd22bf8d2091ad0a
killertofus <cinemassacres@gmail.com>
2023-05-12 18:46:30 +0000

    shortened the code

--- input.py
+++ input.py
@@ -1,20 +1,5 @@
 import sqlite3
 import re
-def get_name(inp_prompt: str, err_prompt: str) -> str:
-        is_valid = re.compile(r"^[a-zA-Z]+$").match
-        while True:
-            name = input(inp_prompt).strip()
-            if not is_valid(name):
-                print(err_prompt)
-                continue
-            return name
-
-def get_user() -> tuple[str, str]:
-    first_name = get_name("First name: ", "Invalid first name")
-    last_name = get_name("Last name: ", "Invalid last name")
-    ID = get_name ("Enter ID: ", "invalid ID")
-    return first_name, last_name, ID
-
 conn = sqlite3.connect('customer.db')
 cur = conn.cursor()
 

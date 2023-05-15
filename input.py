46c9b88b3c080e52485d1152cd349a5e54852309
killertofus <cinemassacres@gmail.com>
2023-05-15 00:17:49 +0000

    now it has input validation

--- input.py
+++ input.py
@@ -4,7 +4,13 @@ conn = sqlite3.connect('customer.db')
 cur = conn.cursor()
 
 first_name = input("enter your first name ")
+while not re.match("[A-Za-z]+", first_name):
+    print("Only letters are allowed")
+    first_name = input("enter your first name ")
 last_name = input("enter your last name ")
+while not re.match("[A-Za-z]+", last_name):
+    print("Only letters are allowed")
+    last_name = input("enter your last name ")
 email = input ("enter your email name ")
 
 cur.execute(f"""

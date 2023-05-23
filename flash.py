579430eba57f2eff809a3fd3676954af24a85a5e
killertofus <cinemassacres@gmail.com>
2023-05-23 21:15:37 +0000

    now it outputs to a db

--- flash.py
+++ flash.py
@@ -1,6 +1,8 @@
 import sqlite3
 import random
 from flask import Flask, session, render_template, request, g
+conn = sqlite3.connect('customer.db')
+cur = conn.cursor()
 
 app = Flask(__name__)
 app.secret_key = "select_a_COMPLEX_secret_key_please"
@@ -8,10 +10,17 @@ app.secret_key = "select_a_COMPLEX_secret_key_please"
 @app.route("/", methods=["POST","GET"])
 
 def home():
+    conn = sqlite3.connect('customer.db')
+    cur = conn.cursor()    
+
     if request.method=="POST":
         email=request.form["email"]
+        cur.execute("insert into customer (email)  values(?)", [email])
         first_name=request.form["first_name"]
+        cur.execute("insert into customer (first_name) values(?)", [first_name]) 
         last_name=request.form["last_name"]
+        cur.execute("insert into customer (last_name) first_name values(?)", [last_name])
+        conn.commit()
         return render_template("output.html", first_name =first_name, last_name=last_name, email=email)
     else:
         return render_template("index.html")

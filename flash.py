8a40268d3e8b36315d0ed9c64fee2431a95aa548
killertofus <cinemassacres@gmail.com>
2023-06-10 20:52:55 +0000

    now it sends a email when you complete the form

--- flash.py
+++ flash.py
@@ -1,6 +1,12 @@
 import sqlite3
+import smtplib
 import random
+import os
+from dotenv import load_dotenv
 from flask import Flask, session, render_template, request, g
+
+load_dotenv()
+
 conn = sqlite3.connect('customer.db')
 cur = conn.cursor()
 
@@ -8,6 +14,7 @@ app = Flask(__name__)
 app.secret_key = "select_a_COMPLEX_secret_key_please"
 
 @app.route("/", methods=["POST","GET"])
+
 
 def home():
     conn = sqlite3.connect('customer.db')
@@ -18,6 +25,11 @@ def home():
         first_name=request.form["first_name"]
         last_name=request.form["last_name"]
         cur.execute("insert into customer (last_name, first_name , email) values(?, ?, ?)", [last_name, first_name , email]);
+        message = ("test")
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login("cinemassacres@gmail.com", os.getenv("password"))
+        server.sendmail("cinemassacres@gmail.com", email, message)
         conn.commit()
         return render_template("output.html", first_name =first_name, last_name=last_name, email=email)
     else:
